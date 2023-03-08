e867ca79b5fcf753a9a23305aaeeecedd00f16c1
matheus <matheussss6613@gmail.com>
2023-03-08 18:26:08 -0300

    Fim

--- src/controller/MatrizThread.java
+++ src/controller/MatrizThread.java
@@ -1,5 +1,48 @@
 package controller;
 
-public class MatrizThread {
+import java.util.Random;
 
+public class MatrizThread 
+{
+	
+	public int[][] matriz()
+	{
+		Random r = new Random();
+		
+		int m[][] = new int[3][5];
+		
+		for(int i = 0; i < 3; i++)
+		{
+			for(int j = 0; j < 5; j++)
+			{
+				m[i][j] = r.nextInt(11) + 1;
+			}
+		}
+
+		return m;	
+	}
+	
+	
+	public void somaThread(int[][] m, int num)
+	{
+		new Thread()
+		{
+			public void run()
+			{
+				int soma = 0;
+				for(int i = 0; i < 5; i++)
+				{
+					soma += m[num][i];
+				}
+				System.out.println(num + " - " + soma);
+			}
+		}.start();
+	}
+	
+	
+	
+	
+	
+	
+	
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,5 +1,24 @@
 package view;
 
-public class Main {
+import controller.MatrizThread;
 
+public class Main 
+{
+	public static void main(String[] args)
+	{
+		
+		MatrizThread controll = new MatrizThread();
+		
+		int m[][] = new int[3][5];
+		
+		m = controll.matriz();
+		
+		for(int i = 0; i < 3; i++)
+		{
+			controll.somaThread(m, i);
+		}
+		
+		
+		
+	}
 }
